1dc35f31501128710ca947ab973113f62ccf3647
gcy <gcy@zhihu.com>
2022-03-14 18:48:34 +0800

    init && add token.sol

---
+++ contracts/Token.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Token {
+    string public name = "My Hardhat token";
+    string public symbol = "MHT";
+
+    uint256 public totalSupply = 1000000;
+
+    // 项目部署人
+    address public owner;
+
+    mapping(address => uint256) balances;
+
+    constructor() {
+        // 所有金额存放在合约部署人手里
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address to, uint256 amount) external {
+        require(balances[msg.sender] >= amount, "Not enough tokens");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+}
