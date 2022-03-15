f6bc33b72221cb9efdbde03db64f493b0a09d29d
gcy <gcy@zhihu.com>
2022-03-15 10:26:11 +0800

    fix deploy to rankeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,10 +15,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 
 // only for test !!!
-const ALCHEMY_API_KEY = "KhvAlJNUI6Jr2REwlshvP-JF9A1lDO4g";
+const ALCHEMY_API_KEY = "1JfL234rSnb0Doq-GjtdjcBgpnY6gTKp";
 
 // only for test !!!
-const ROPSTEN_PRIVATE_KEY = "2da7ee040d206c0ae3c7ca9db8238c06b1457bbc520bf180a9a81db6a508e491";
+const RANKEBY_PRIVATE_KEY = "2da7ee040d206c0ae3c7ca9db8238c06b1457bbc520bf180a9a81db6a508e491";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -26,9 +26,9 @@ const ROPSTEN_PRIVATE_KEY = "2da7ee040d206c0ae3c7ca9db8238c06b1457bbc520bf180a9a
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`${ROPSTEN_PRIVATE_KEY}`]
+    rankeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${RANKEBY_PRIVATE_KEY}`]
     }
   }
 };
